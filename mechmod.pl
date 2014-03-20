ae745400b8615d5f9f262e78ed4da77f3558f5ec
sue7ga <sue77ga@gmail.com>
2014-03-20 14:36:42 -0700

    test files

--- mechmod.pl
+++ mechmod.pl
@@ -23,15 +23,21 @@ $mech->success or die "postに失敗しました:",$mech->response->status_line;
 
 my @links = $mech->find_all_links(url_regex => qr/\.tar\.gz$/);
 
-my @urls = map{ $_->[0] }@links;
-
-print "exists Donwload ",scalar @urls,"tall ball","\n";
-
 use Data::Dumper;
+
+my @urls = map{$_->[0]}@links;
+my @test_urls = grep{$_ =~ /Test/}map{$_->[0]}@links;
+my @not_test_urls = grep{$_ !~ /Test/}map{ $_->[0] }@links;
+
+print "test files",scalar @test_urls,"\n";
+print "not test files",scalar @not_test_urls,"\n";
+print "exists Donwload",scalar @urls,"tall ball","\n";
+
+print "fooooo!" unless scalar @urls == scalar @test_urls + scalar @not_test_urls;
 
 foreach my $url(@urls){
   my $filename = basename($url);
   print "$filename-->";
-  $mech->get($url,':content_file'=>$filename);
+  #$mech->get($url,':content_file'=>$filename);
   print -s $filename,"bytes","\n";
 }
